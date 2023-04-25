f05e1357c1008a0844194cb84c8fd20b2e60b399
Muhammad Hassan <60579417+hassan1344@users.noreply.github.com>
2023-04-25 12:34:23 +0500

    added table names

--- news_aggregator/aggregator/models.py
+++ news_aggregator/aggregator/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 class NewsArticle(models.Model):
+    class Meta:
+        db_table="news_article"
+
     title = models.CharField(max_length=200)
     description = models.TextField()
     url = models.URLField()
     source = models.CharField(max_length=50)
 
 class UserFavorite(models.Model):
+    class Meta:
+        db_table="user_favorites"
+        
     user = models.CharField(max_length=50)
     article = models.ForeignKey(NewsArticle, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
