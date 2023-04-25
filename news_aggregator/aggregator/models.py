3d8af8a27aa6747800da90596273af3b52e1db6e
Muhammad Hassan <60579417+hassan1344@users.noreply.github.com>
2023-04-25 12:32:51 +0500

    added database models
    - for news article and favorites

--- news_aggregator/aggregator/models.py
+++ news_aggregator/aggregator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class NewsArticle(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    url = models.URLField()
+    source = models.CharField(max_length=50)
+
+class UserFavorite(models.Model):
+    user = models.CharField(max_length=50)
+    article = models.ForeignKey(NewsArticle, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
